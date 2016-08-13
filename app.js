e0aa977eea756fbf4d704f966f4483625ee1cf5d
Ramsés López <ramses.lopezs@gmail.com>
2016-08-13 16:19:39 -0400

    First version of the json complete

--- app.js
+++ app.js
@@ -3,11 +3,34 @@ var inspect = require('util').inspect
 var _log = (t) => console.log(inspect(t, {color: true, depth: Infinity}))
 
 var dict = [['label','en','es','fr'],
-	['casa','home','casa','maison'],
-	['perro','dog','perro','chien'],
-	['gato','cat','gato','chat'],
-	['mujer','woman','mujer','femme'],
-	['hombre','man','hombre','homme'],
-	['carro','car','carro','voiture']]
+	['CASA','home','casa','maison'],
+	['PERRO','dog','perro','chien'],
+	['GATO','cat','gato','chat'],
+	['MUJER','woman','mujer','femme'],
+	['HOMBRE','man','hombre','homme'],
+	['CARRO','car','carro','voiture']]
 
-_log(dict[0])
+var result = dict.reduce((langFile,row,idx) => {
+
+	//header processing
+	if(idx == 0){
+		row.forEach((e, rowIdx) => {
+			if(rowIdx > 0) langFile[e] = {}
+		})
+
+		return langFile
+	}
+	else{
+		Object.keys(langFile).map((lang, i) => {
+			var ii = i+1
+			langFile[lang][row[0]] = row[ii]
+		})
+	}
+
+
+	return langFile
+
+}, {})
+
+console.log('=========================================================')
+_log(result)
