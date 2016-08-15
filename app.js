cc5c9019895dc1caf35c3c82807145722cdec300
Ramsés López <ramses.lopezs@gmail.com>
2016-08-14 21:48:27 -0400

    Added csv file reading

--- app.js
+++ app.js
@@ -4,21 +4,14 @@ var inspect = require('util').inspect
 
 var _log = (t) => console.log(inspect(t, {colors: true, depth: Infinity}))
 
-var dict = [['label','en','es','fr'],
-	['CASA','house','casa','maison'],
-	['CASA.PUERTA','door','puerta','porte'],
-	['CASA.PISO','floor','piso','sol'],
-	['CASA.MUEBLES.SOFA','couch','sofa','canapé'],
-	['CASA.MUEBLES.CAMA','bed','cama','lit'],
-	['CASA.MUEBLES.MESA','table','mesa','maison'],
-	['PERRO','dog','perro','chien'],
-	['GATO','cat','gato','chat'],
-	['MUJER','woman','mujer','femme'],
-	['HOMBRE','man','hombre','homme'],
-	['CARRO','car','carro','voiture']]
+var dict =
+	fs.readFileSync('data/dictionary.csv', 'utf8')
+	.split('\n')
+	.map((row) => row.split(','))
+
+console.log('file read')
 
 var translations = dict.reduce((langFile, row, idx) => {
-
 	//header processing
 	if(idx == 0){
 		row.forEach((e, rowIdx) => {
@@ -31,19 +24,20 @@ var translations = dict.reduce((langFile, row, idx) => {
 			_.set(langFile[lang], row[0], row[i+1])
 		})
 	}
-
 	return langFile
-
 }, {})
 
 console.log('translations done')
-_log(translations)
-console.log('=========================================================')
+
+// _log(translations)
+// console.log('=========================================================')
+
 console.log('writing files')
 
 Object.keys(translations).forEach((lang) => {
 	var data = translations[lang]
 	fs.writeFileSync(`lang/${lang}.json`, JSON.stringify(data))
+	console.log(`  written lang/${lang}.json`)
 })
 
 console.log('done!')
